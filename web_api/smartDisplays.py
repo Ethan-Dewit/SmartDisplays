11b67b808f1249b74cde5c758a251b0f40e8ceec
Lauren Bruckstein <77750946+laurenbruckstein@users.noreply.github.com>
2022-03-20 15:39:44 -0400

    API outline with more detail

--- web_api/smartDisplays.py
+++ web_api/smartDisplays.py
@@ -7,12 +7,19 @@ import matplotlib.pyplot as plt
 class smartDisplays():
 
     def __init__(self):
-        # How are we recieving this information? A json?
+    # Recieving this in a json file
+    # hosting a pandas df on a task application
+
+    # put userIds in a queue
+    # start with 10 constant userIds in df for testing
+    # iterate through shapes in a for loop to generate userIds
+    # df should include userID, direction, display, and time
 
 
     # Get userID from app and store in a dataframe
     def get_available_id(self, userId):
-
+        # change userID symbols at some point
+        
 
     # Clear out any ideas that don't need more directions
     # Set a time limit from the last time each idea was used
@@ -27,6 +34,4 @@ class smartDisplays():
     # the tablet we call
     # tablet going to
     def send_direction(self, userId):
-
         # return direction
-
